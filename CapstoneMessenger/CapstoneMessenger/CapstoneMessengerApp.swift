ffd543205ae446594e56e73415d0a189bcf81bb7
Kyle McInnis <kyle.ap.mcinnis@gmail.com>
2022-01-20 02:59:54 -0400

    First test commit new project git

---
+++ CapstoneMessenger/CapstoneMessenger/CapstoneMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneMessengerApp.swift
+//  CapstoneMessenger
+//
+//  Created by Kyle McInnis on 2022-01-20.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
